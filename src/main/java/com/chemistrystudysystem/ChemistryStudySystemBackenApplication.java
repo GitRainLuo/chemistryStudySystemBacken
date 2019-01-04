6173ba234ce15569d3fa8ca7ab9b61e598ccf23c
hmj <1004033345@qq.com>
2019-01-04 17:00:29 +0800

    初始化项目

---
+++ src/main/java/com/chemistrystudysystem/ChemistryStudySystemBackenApplication.java
@@ -0,0 +1,14 @@
+package com.chemistrystudysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChemistryStudySystemBackenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChemistryStudySystemBackenApplication.class, args);
+	}
+
+}
+
